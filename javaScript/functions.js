bc1a2868bd28e544da28795745e557ce833e6b20
Mordasan Alexandru-Ioan <mordasanalex@yahoo.com>
2021-09-28 19:10:44 +0300

    updated js and css

--- javaScript/functions.js
+++ javaScript/functions.js
@@ -1,21 +1,30 @@
 let eventMenuClick = e => {
     let obj = e.target;
     let b = document.querySelector('b');
+    let aside = document.querySelector('aside');
 
 
     if (obj.tagName == "I" && b.className == "closed") {
         let button = obj.parentNode;
         button.classList.remove("closed");
         button.classList.add("opened");
+        aside.classList.remove("closed");
+        aside.classList.add("opened");
     } else if (obj.tagName == "B" && obj.className == "closed") {
         obj.classList.remove("closed");
         obj.classList.add("opened");
+        aside.classList.remove("closed");
+        aside.classList.add("opened");
     } else if (obj.tagName == "I" && b.className == "opened") {
         let button = obj.parentNode;
         button.classList.remove("opened");
         button.classList.add("closed");
+        aside.classList.remove("opened");
+        aside.classList.add("closed");
     } else if (obj.tagName == "B" && obj.className == "opened") {
         obj.classList.remove("opened");
         obj.classList.add("closed");
+        aside.classList.remove("opened");
+        aside.classList.add("closed");
     }
 }
